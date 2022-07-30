c39e9f658ae8b9bbd033328b38e0a73e0b4f42c1
Man dev <nodirbekqobilov332@gmail.com>
2022-07-30 19:39:28 +0500

    Contact error remove

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import TelegramBot from "node-telegram-bot-api";
 import geoFinder from "../utils/geoFinder.js";
 import func from "../utils/FS.js";
 import keyboards from "../utils/keyboards.js";
+
 const bot = new TelegramBot(process.env.API_KEY, {
   polling: true,
 });
@@ -16,6 +17,7 @@ let con = 0;
 let chat = 0;
 let Adress = "";
 let contact = "";
+let image = false;
 let filePath = "";
 let old_order = false;
 let productlar = [];
@@ -46,103 +48,119 @@ async function toMessageAdmin(arg) {
   bot.sendMessage(admin, xabar);
 }
 
-bot.on("message", (msg) => {
-  console.log(msg);
-  chat += 1;
-  const chat_id = msg.from.id;
-  if (msg.text == "RO'YXAT 📋") {
-    productlar = [];
-    if (msg.from.id == admin) {
-      console.log("admin");
-      bot.sendMessage(chat_id, "Siz 👮‍♂️ (ADMIN)", {
+bot.on("message", async (msg) => {
+  try {
+    chat += 1;
+    const chat_id = msg.from.id;
+    if (msg.text == "RO'YXAT 📋") {
+      productlar = [];
+      if (msg.from.id == admin) {
+        bot.sendMessage(chat_id, "Siz 👮‍♂️ (ADMIN)", {
+          reply_markup: {
+            keyboard: keyboards.admin,
+            resize_keyboard: true,
+          },
+        });
+      } else {
+        bot.sendMessage(chat_id, "Sovg'alar 🎁", {
+          reply_markup: {
+            keyboard: keyboards.royxat,
+            resize_keyboard: true,
+          },
+        });
+      }
+    } else if (msg.text == "Ko'rish") {
+      let data = func.read("Order");
+      data.forEach((e) => {
+        toMessageAdmin(e);
+      });
+    } else if (
+      msg.text == "Qo'shish" ||
+      msg.text == "O'chirish" ||
+      msg.text == "O'zgartirish"
+    ) {
+      bot.sendMessage(
+        chat_id,
+        "Hozircha " + msg.text.toLowerCase() + " imkonsiz"
+      );
+
+      // bot.sendMessage(admin, "Mahsulot bo'limini tanlang!", {
+      //   reply_markup: {
+      //     keyboard: keyboards.royxat2,
+      //     resize_keyboard: true,
+      //   },
+      // });
+    } else if (msg.from.id == admin && msg.text[0] == "+") {
+      if (msg.text.length == 13) {
+        let data = func.read("Order");
+        let user = data.find((e) => e.contact == msg.text);
+        if (user) toMessageAdmin(user);
+        else bot.sendMessage(chat_id, "Topilmadi❗️");
+      } else {
+        bot.sendMessage(chat_id, "Xato ❌");
+      }
+      // } else if (
+      //   msg.text == "Joynamoz+" ||
+      //   msg.text == "Quron+" ||
+      //   msg.text == "Nabor+" ||
+      //   msg.text == "Boshqa+"
+      // ) {
+      //   filePath = msg.text.slice(0, msg.text.length - 1);
+      //   bot.sendMessage(chat_id, "Hozircha qo'sha olmaysiz!");
+      //   image = true;
+    } else if (
+      msg.text == "Joynamoz" ||
+      msg.text == "Quron" ||
+      msg.text == "Nabor" ||
+      msg.text == "Boshqa"
+    ) {
+      filePath = msg.text;
+      let datas = func.read(msg.text);
+      let orders = func.read("Order");
+      let desc = ``;
+      let button = [];
+      let buttons = [];
+      for (let data of datas) {
+        let { id, name, title } = data;
+        desc +=
+          id + ":    <strong>" + name + "</strong>   <i>(" + title + ")</i> \n";
+        button.push({ text: id + "", callback_data: id + "" });
+        if (button.length == 5) {
+          buttons.push(button);
+          button = [];
+        }
+      }
+      buttons.push(button);
+      let findUser = orders.find((el) => el.user_id == msg.from.id);
+      old_order = findUser ? true : false;
+      if (findUser?.products.length) {
+        buttons.push([
+          {
+            text: "Mening   🛍 larim",
+            callback_data: "Order",
+          },
+        ]);
+        con = 1;
+      }
+
+      bot.sendPhoto(chat_id, "./images/" + filePath + "/main.jpg", {
+        caption: desc,
+        parse_mode: "HTML",
         reply_markup: {
-          keyboard: keyboards.admin,
-          resize_keyboard: true,
+          inline_keyboard: buttons,
         },
       });
-    } else {
+    } else if (msg.text == "Ortga") {
       bot.sendMessage(chat_id, "Sovg'alar 🎁", {
         reply_markup: {
-          keyboard: keyboards.royxat,
+          keyboard: [keyboards.menu],
           resize_keyboard: true,
         },
       });
+      chat = 0;
     }
-  } else if (msg.text == "Ko'rish") {
-    let data = func.read("Order");
-    data.forEach((e) => {
-      toMessageAdmin(e);
-    });
-  } else if (msg.text == "Qo'shish") {
-    console.log("qo'shish");
-    bot.sendMessage(admin, "Mahsulot bo'limini tanlang!", {
-      reply_markup: {
-        keyboard: keyboards.royxat2,
-        resize_keyboard: true,
-      },
-    });
-  } else if (msg.text[0] == "+") {
-    if (msg.text.length == 13) {
-      let data = func.read("Order");
-      let user = data.find((e) => e.contact == msg.text);
-      if (user) toMessageAdmin(user);
-      else bot.sendMessage(chat_id, "Topilmadi❗️");
-    } else {
-      bot.sendMessage(chat_id, "Xato ❌");
-    }
-  } else if (
-    msg.text == "Joynamoz+" ||
-    msg.text == "Quron+" ||
-    msg.text == "Nabor+" ||
-    msg.text == "Boshqa+"
-  ) {
-  } else if (
-    msg.text == "Joynamoz" ||
-    msg.text == "Quron" ||
-    msg.text == "Nabor" ||
-    msg.text == "Boshqa"
-  ) {
-    filePath = msg.text;
-    let datas = func.read(msg.text);
-    let orders = func.read("Order");
-    let desc = ``;
-    let button = [];
-    let buttons = [];
-    for (let data of datas) {
-      let { id, name, title } = data;
-      desc +=
-        id + ":    <strong>" + name + "</strong>   <i>(" + title + ")</i> \n";
-      button.push({ text: id + "", callback_data: id + "" });
-      if (button.length == 5) {
-        buttons.push(button);
-        button = [];
-      }
-    }
-    buttons.push(button);
-    let findUser = orders.find((el) => el.user_id == msg.from.id);
-    old_order = findUser ? true : false;
-    if (findUser?.products.length) {
-      buttons.push([
-        {
-          text: "Mening   🛍 larim",
-          callback_data: "Order",
-        },
-      ]);
-      con = 1;
-    }
-
-    bot.sendPhoto(chat_id, "./images/" + filePath + "/main.jpg", {
-      caption: desc,
-      parse_mode: "HTML",
-      reply_markup: {
-        inline_keyboard: buttons,
-      },
-    });
-  } else if (msg.text == "Ortga") {
-    for (let i = 0; i <= chat; i++) {
-      if (msg?.message_id - i) bot.deleteMessage(chat_id, msg.message_id - i);
-    }
-    chat = 0;
+  } catch (e) {
+    console.log(e);
   }
 });
 
@@ -243,9 +261,9 @@ bot.on("callback_query", async (msg) => {
         );
         data.push(obj);
         func.write("Order", data);
+        console.log(msg.message.message_id);
         toMessageAdmin(obj);
-        bot.deleteMessage(chat_id, msg.message.message_id);
-        bot.sendMessage(chat_id, "✅ Siz bilan Admin bog'lanadi!", {
+        await bot.sendMessage(chat_id, "✅ Siz bilan Admin bog'lanadi!", {
           reply_markup: {
             keyboard: [[{ text: "RO'YXAT 📋" }]],
             resize_keyboard: true,
@@ -345,10 +363,6 @@ bot.on("callback_query", async (msg) => {
         },
       });
     } else if (msg.data == "Ortga") {
-      for (let i = 0; i <= chat; i++) {
-        if (msg?.message_id - i) bot.deleteMessage(chat_id, msg.message_id - i);
-      }
-      chat = 0;
       con = 0;
     }
   } catch (err) {
@@ -357,41 +371,49 @@ bot.on("callback_query", async (msg) => {
 });
 
 bot.on("contact", (msg) => {
-  chat += 1;
   const chat_id = msg.from.id;
-  contact = "+" + msg.contact.phone_number;
-  bot.sendMessage(chat_id, "Joylashuvni jo'nating", {
-    reply_markup: {
-      keyboard: keyboards.location,
-      resize_keyboard: true,
-      one_time_keyboard: true,
-    },
-  });
+  if (productlar.length) {
+    chat += 1;
+    contact = "+" + msg.contact.phone_number;
+    bot.sendMessage(chat_id, "Joylashuvni jo'nating", {
+      reply_markup: {
+        keyboard: keyboards.location,
+        resize_keyboard: true,
+        one_time_keyboard: true,
+      },
+    });
+  } else {
+    bot.sendMessage(chat_id, "Avval mahsulot tanlang!");
+  }
 });
 
 bot.on("location", async (msg) => {
-  chat += 1;
   const chat_id = msg.from.id;
-  const { latitude, longitude } = msg.location;
-  let locatsiya = await geoFinder(latitude, longitude);
-  Adress = locatsiya.results[0].formatted;
-  bot.sendMessage(chat_id, "Manzilingiz: " + Adress + " ni tasdiqlang!", {
-    reply_markup: {
-      inline_keyboard: [
-        [
-          {
-            text: "✅",
-            callback_data: "loc_ok",
-          },
-          {
-            text: "❌",
-            callback_data: "X",
-          },
+  if (productlar.length) {
+    chat += 1;
+    const { latitude, longitude } = msg.location;
+    let locatsiya = await geoFinder(latitude, longitude);
+    Adress = locatsiya.results[0].formatted;
+    bot.sendMessage(chat_id, "Manzilingiz: " + Adress + " ni tasdiqlang!", {
+      reply_markup: {
+        inline_keyboard: [
+          [
+            {
+              text: "✅",
+              callback_data: "loc_ok",
+            },
+            {
+              text: "❌",
+              callback_data: "X",
+            },
+          ],
         ],
-      ],
-      resize_keyboard: true,
-      one_time_keyboard: true,
-    },
-  });
-  con = 1;
+        resize_keyboard: true,
+        one_time_keyboard: true,
+      },
+    });
+    con = 1;
+  } else {
+    bot.sendMessage(chat_id, "Avval mahsulot tanlang!");
+  }
 });
